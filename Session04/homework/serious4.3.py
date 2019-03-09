a5150a083809e2509629643e0d8631bc8cd88202
ChiHieu <nnch013674350@gmail.com>
2019-03-09 16:24:19 +0700

    initial commit

--- Session04/homework/serious4.3.py
+++ Session04/homework/serious4.3.py
@@ -1,27 +1,26 @@
 print('quiz')
 question = 'if x = 8, then what is the value of 4(x + 3)?\n'
-options = 'a. 35\nb. 36\nc. 40\nd. 44\n'
+options = '1. 35\n2. 36\n3. 40\n4. 44\n'
 print(question)
 print(options)
 
 while True:
-    choice = input("please choose 'a', 'b', 'c', 'd'\n")
+    choice = input("please choose '1', '2', '3', '4'\n")
 
-    if choice == 'd':
+    if choice == '4':
         print('bingo')
         break
     else: print(':(')
     while True:
-        choice = input("please choose again 'a', 'b', 'c', 'd'\n")
-        if choice == 'd':
+        choice = input("please choose again '1', '2', '3', '4'\n")
+        if choice == '4':
             print('bingo')
             break
         else:
             print(':(\nYou ran out of your attempts')
             stop = True
             break
-    if stop:
-        break
+    break 
 
 
 
